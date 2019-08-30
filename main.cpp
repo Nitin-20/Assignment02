0a0e047a7f985148be1cfdc2f33272fb33e2a75d
Nitin-20 <54406101+Nitin-20@users.noreply.github.com>
2019-08-30 11:47:17 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
+/*3. Create Your own String class (Use class name as
+MyString) with data members
+
+char * p;
+int size;
+
+Allocate memory dynamically for pointer and get
+size from user.
+Implement followings
+ Default constructor
+ Parameterized constructor
+ Copy Constructor
+ Destructor for clean-up (use delete compulsory)
+ Display function.
+ Separate header and implementation files*/
 #include"Mystring.h"
 int main()
 {
